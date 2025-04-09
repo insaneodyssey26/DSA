ad754e4c08cc381da5187b8fc9ae0df967df7cf2
Masum <hydrapikachu29@gmail.com>
2025-04-10 00:30:18 +0530

    Added Subsets - 41

--- Subsets.cpp
+++ Subsets.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> res;
-        vector<int> subset;
-        createSubset(nums,0,res,subset);
-        return res;
+        vector<vector<int>> result;
+        vector<int> current;
+        createSubset(0, nums, current, result);
+        return result;
     }
-    void createSubset(vector<int>& nums,int index, vector<vector<int>>& res,vector<int>& subset)
-    {
-        if(index==nums.size())
-        {
-            res.push_back(subset);
-            return;
+
+    void createSubset(int st, vector<int>& nums, vector<int>& current, vector<vector<int>>& result) {
+        result.push_back(current);
+        for (int i = st; i < nums.size(); i++) {
+            current.push_back(nums[i]);
+            createSubset(i + 1, nums, current, result);
+            current.pop_back(); 
         }
-        subset.push_back(nums[index]);
-        createSubset(nums,index+1,res,subset);
-        subset.pop_back();
-        createSubset(nums,index+1,res,subset);
-    }
+    }
 };
